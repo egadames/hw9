8ff98bccc2a0bfe5ea8aa589c9cdc84f55279f9d
Kire Semada <egadames990@gmail.com>
2020-04-22 22:19:41 -0700

    starting heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const database = require("./db/db.json");
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 3003;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
